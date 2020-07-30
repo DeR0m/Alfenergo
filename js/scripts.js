f1b17288fc3cff8720d3deb83161c6bbe1bb429d
DeR0m <denisromantsov@mail.ru>
2020-07-31 00:36:20 +0300

    update img service

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ $(document).ready(function(){
         $('body').toggleClass('lock');
     });
 });
+
+function ibg(){
+
+    let ibg=document.querySelectorAll(".ibg");
+        for (var i = 0; i < ibg.length; i++) {
+            if(ibg[i].querySelector('img')){
+            ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+            }
+        }
+}
+    
+    ibg();
